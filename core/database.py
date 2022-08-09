a2a12aa08a0cd71414e39f0c072423c720f23d10
iIlyaM <imoldavskiy@yandex.ru>
2022-08-09 15:33:03 +0300

    - add aws config to database.py.

--- core/database.py
+++ core/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:6201111@localhost/practice_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:6201111mmm@practice.cyptanmb1ecn.eu-central-1.rds.amazonaws.com" \
+                          "/practice_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL)

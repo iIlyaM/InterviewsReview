2e12948ec8d4c6815201ea0782eb2d173ab0e9c6
iIlyaM <imoldavskiy@yandex.ru>
2022-08-10 00:35:25 +0300

    -fix docker

--- app/docker/docker_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Response, status
-
-docker_router = APIRouter()
-
-#
-# @docker_router.get("/healthcheck")
-# async def get(request, response_class=Response):
-#     return status.HTTP_200_OK
--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,11 @@ from fastapi import APIRouter
 from app.record import record_router
 from app.users import users_router
 from app.auth import router
-# from app.docker import docker_router
 
 routes = APIRouter()
 
 routes.include_router(router.router, prefix="/reviews")
 routes.include_router(users_router.router, prefix="/reviews/users")
 routes.include_router(record_router.record_router, prefix="/users/records")
-# routes.include_router(docker_router.docker_router, prefix="/docker")
 
 
